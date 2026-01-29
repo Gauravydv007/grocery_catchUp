f0b1b9ebd0a8ed5ac195bd59e25aa0fa7d59db36
Gaurav Yadav <Gaurav88689548@gmail.com>
2026-01-29 12:43:10 +0530

    Generate Folder Stucture

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:grocery_app/features/home/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black87),
+
+      ),
+      home: Home(),
+
+
+    );
+  }
+}
